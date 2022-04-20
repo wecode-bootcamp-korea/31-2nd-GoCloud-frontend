873c565a930bd69ee999aa6a9c8396ee2ec1b0fe
노 <shduddhks1234@naver.com>
2022-04-20 21:37:39 +0900

    ADD: API 추가

--- src/config.js
+++ src/config.js
@@ -1,8 +1,11 @@
 export const BASE_URL = 'http://10.58.7.67:8000';
 
 export const API = {
-  kakaologin: `${BASE_URL}/users/signin/kakao`,
-  hostconvert: `${BASE_URL}/users/host`,
-  spacelist: `${BASE_URL}/spaces/lists`,
-  spacesreviews: `${BASE_URL}/spaces/reviews`,
+  kakaologin: `${BASE_URL}/users/signin/kakao`, // 카카오 로그인
+  hostconvert: `${BASE_URL}/users/host`, // 호스트 등록
+  list: `${BASE_URL}/spaces`, // 리스트
+  reviews: `${BASE_URL}/spaces/reviews`, // 리뷰
+  detail: `${BASE_URL}/spaces/detail`, // 디테일
+  booking_list: `${BASE_URL}/spaces/bookinglist`, // 예약페이지ㄴ
+  host: `${BASE_URL}/spaces/register`, // 공간등록
 };
