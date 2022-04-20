2609f0d713077d86f84bb76cae82eb2b6d7aba46
노 <shduddhks1234@naver.com>
2022-04-20 12:31:32 +0900

    state 삭제 / 삭제

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Aside from './Aside';
+import Search from './Search';
 import { AiOutlineMenu } from 'react-icons/ai';
-import { GoSearch } from 'react-icons/go';
 
 const Nav = () => {
   const [aside, setAside] = useState(true);
@@ -12,15 +12,7 @@ const Nav = () => {
   return (
     <NavBox>
       <GoGloud>Go-Cloud</GoGloud>
-      <Search>
-        <SearchIcon>
-          <GoSearch />
-        </SearchIcon>
-        <SearchInput
-          type="text"
-          placeholder="지역, 공간유형, 공간명으로 찾아보세여"
-        />
-      </Search>
+      <Search />
       <Menu>
         <div>내 공간 등록하기</div>
         <div>
@@ -48,28 +40,6 @@ const GoGloud = styled.div`
   text-align: left;
   font-size: 35px;
   font-weight: bold;
-`;
-
-const Search = styled.form`
-  position: relative;
-  right: 20%;
-`;
-
-const SearchIcon = styled.div`
-  position: absolute;
-  left: 2%;
-  bottom: 16%;
-  width: 25px;
-  height: 25px;
-  font-size: 25px;
-`;
-
-const SearchInput = styled.input`
-  width: 400px;
-  height: 40px;
-  border-radius: 100px;
-  margin-right: 162px;
-  padding-left: 50px;
 `;
 
 const Menu = styled.div`
